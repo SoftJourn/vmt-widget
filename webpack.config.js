30fecdfca14e0073c6f972a8c24dd5eefd4b49d8
xhoni96 <xhonikomini96@gmail.com>
2022-10-31 16:58:35 +0100

    reverse webpack version upgrade as it causes problems on staging build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
+    entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: './dist/bundle.js',
         libraryTarget: 'umd',
         library: 'VMTWidget',
     },
     module: {
-        rules: [
+        loaders: [
             {
                 test: /\.js?$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-2'],
                 },
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
             },
         ],
     },
+    plugins: [
+        new UglifyJsPlugin(),
+        new webpack.ProvidePlugin({
+            Promise: 'es6-promise-promise', // works as expected
+        }),
+    ],
 };
